918c08b63fc3c4b84257440c9233fa8978edb341
周福\Administrator <275900060@qq.com>
2017-06-06 18:43:41 +0800

    ss

---
+++ src/chineseIdol/Audience.java
@@ -0,0 +1,20 @@
+package chineseIdol;
+
+public class Audience {
+	public void takeSeat() {
+		// TODO Auto-generated method stub
+		System.out.println("观众入座");
+	}
+	public void turnOffCellPhones() {
+		// TODO Auto-generated method stub
+		System.out.println("观众关闭手机");
+	}
+	public void applaud() {
+		// TODO Auto-generated method stub
+		System.out.println("观众鼓掌:啪 啪 啪");
+	}
+	public void demandRefund() {
+		// TODO Auto-generated method stub
+		System.out.println("表演太糟糕了，我们要求退票");
+	}
+}
